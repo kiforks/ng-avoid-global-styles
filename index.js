dc99c659b6e3e2633ff300ac675e8f2e52b1cfbc
Kifor Kostiantyn <k.kifor@tenantcloud.com>
2023-07-31 18:49:59 +0300

    Changed syntax

--- index.js
+++ index.js
@@ -28,49 +28,23 @@ module.exports = stylelint.createPlugin(ruleName, expectation => {
 			return;
 		}
 
-		function precedesParentSelector(current) {
-			do {
-				current = current.next();
-
-				if (current.type === 'nesting') {
-					return true;
-				}
-			} while (current.next());
-
-			return false;
-		}
-
 		// attribute, class, combinator, comment, id, nesting, pseudo, root, selector, string, tag, or universal
 		const chainingTypes = ['attribute', 'class', 'id', 'pseudo', 'tag', 'universal'];
 
 		const interpolationRe = /#{.+?}$/;
 
 		root.walkRules(rule => {
-			parseSelector(rule.selector, result, rule, fullSelector => {
-				let message;
+			const isFristNestedLevel = rule.parent.type === 'root';
+			const isNgDeep = rule.selector.includes('::ng-deep');
 
-				fullSelector.walk(node => {
-					if (expectation === 'always' && node.parent.type === 'root' && node.selector.includes('::ng-deep')) {
-						message = messages.error;
-					}
-
-					if (expectation === 'never') {
-						if (rule.parent.type === 'root' || rule.parent.type === 'atrule') {
-							return;
-						}
-
-						message = messages.rejected;
-					}
-
-					utils.report({
-						ruleName,
-						result,
-						node: rule,
-						message,
-						index: node.sourceIndex,
-					});
+			if (expectation === 'always' && isFristNestedLevel && isNgDeep) {
+				utils.report({
+					ruleName,
+					result,
+					node: rule,
+					message: messages.error,
 				});
-			});
+			}
 		});
 	};
 });
